8b8a5597dd218a4f9b827784c9230c6515fb23bb
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2021-04-07 19:04:12 +0100

    THRET-35: Generate Spring Workspace for Blog Service

---
+++ src/main/java/com/thretclothing/blog/ThretBlogApplication.java
@@ -0,0 +1,12 @@
+package com.thretclothing.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThretBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThretBlogApplication.class, args);
+	}
+}
